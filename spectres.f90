756aae0a5d10285e874e69e915f12d2d3d0e3c1d
François Labbé <francois.labbe@univ-lemans.fr>
2016-05-27 15:28:49 +0200

    ajout de la routine  poids

--- spectres.f90
+++ spectres.f90
@@ -10,6 +10,7 @@ module spectres
   real(dp)::Q(N,42)
   real(dp)::Y(N)
   real(dp)::BF(N)
+  real(dp)::P(N)  ! poids statistique des canaux, si poids(i)=0 le canal i est ignoré.  
   integer::NS ! nombre de sous-spectres theoriques utilisé pour l'ajsutement d'un spectre experimental.
   contains
   !---------------------------------------------------------------------
@@ -21,5 +22,22 @@ module spectres
     brumoy=0.1_dp*sum(BF(1:10))
     BF=BF-brumoy
   end subroutine spectres_preparer_bruit
-  
+  !---------------------------------------------------------------------
+  subroutine spectres_poids(iz)
+  ! met à zero le poids statistique des canaux à ignorer
+    integer,intent(in)::iz(10)
+    integer::i,k
+    canaux : do i=2,N
+      do k=1,9,2
+        if((i>= IZ(k)) .AND. (i<=IZ(k+1))) then
+          P(i)=0.0_dp
+          cycle canaux
+        endif
+      enddo
+    enddo canaux
+  end subroutine spectres_poids
+  !---------------------------------------------------------------------
+  subroutine spectres_calculer
+    
+  end subroutine spectres_calculer
 end module spectres
